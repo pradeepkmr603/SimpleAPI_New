324d220f635472f58e6af556e18f1a7b667b161f
Pradeep Kumar <pradeep.kumar4@pathinfotech.com>
2024-07-30 00:26:32 +0530

    third commit

--- Test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
+++ Test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0a09136fcd5ea93978b81847dec177911eab8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adf87edb0524f3842a2198064724902b5382c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
+++ src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2e57c38bfb85eeee2b531b4a9a2aef175dcc5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adf87edb0524f3842a2198064724902b5382c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
